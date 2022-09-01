e69543b77d4ff16ac32e979921657e6312cdf610
kozlov211 <sayf1ck64.7@gmail.com>
2022-09-01 10:08:46 +0300

    single_linked_list

--- single-linked-list/main.cpp
+++ single-linked-list/main.cpp
@@ -1,13 +1,5 @@
 #include "tests.h"
-#include "single_linked_list.h"
-#include "examples.h"
-
-using namespace single_linked_list;
 
 int main() {
     tests_single_linked_list::AllTests();
-
-    /// operator =
-
-
 }
