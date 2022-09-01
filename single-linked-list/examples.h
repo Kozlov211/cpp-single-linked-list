1c81976226c05005af5afd96ffcef25accbc46ab
kozlov211 <sayf1ck64.7@gmail.com>
2022-09-01 10:12:18 +0300

    single_linked_list

--- single-linked-list/examples.h
+++ single-linked-list/examples.h
@@ -10,45 +10,45 @@ namespace examples_use_single_linked_list {
 
 void Contsructor() {
     SingleLinkedList<std::string> words1 {"the", "frogurt", "is", "also", "cursed"};
-    SingleLinkedList<std::string> words2(words1);
-    SingleLinkedList<std::string> words3;
+    SingleLinkedList<std::string> words2(words1); // {"the", "frogurt", "is", "also", "cursed"}
+    SingleLinkedList<std::string> words3; // {}
 }
 
 void EqualsOperator() {
     SingleLinkedList<std::string> words1 {"the", "frogurt", "is", "also", "cursed"};
-    SingleLinkedList<std::string> words2;
-    words2 = words1;
+    SingleLinkedList<std::string> words2; // {}
+    words2 = words1; // {"the", "frogurt", "is", "also", "cursed"}
 }
 
 void Swap() {
     SingleLinkedList<int> list1{1, 2, 3};
     SingleLinkedList<int> list2 {4, 5};
-    list1.swap(list2);
+    list1.swap(list2); // list1{4,5} list2{1,2,3}
 }
 
 void PopFront() {
     SingleLinkedList<char> chars{'A', 'B', 'C', 'D'};
-    chars.PopFront();
+    chars.PopFront(); // chars{'B', 'C', 'D'}
 }
 
 void PushFront() {
-    SingleLinkedList<std::string> letters;
-    letters.PushFront("abc");
+    SingleLinkedList<std::string> letters; // {}
+    letters.PushFront("abc"); // letters{"abc"}
 }
 
 void Clear() {
     SingleLinkedList<char> chars{'A', 'B', 'C', 'D'};
-    chars.Clear();
+    chars.Clear(); // chars{}
 }
 
 void GetSize() {
     SingleLinkedList<double> list{1.2, 2.7};
-    list.GetSize();
+    list.GetSize(); // 2
 }
 
 void IsEmpty() {
     SingleLinkedList<double> list{1.2, 2.7};
-    list.IsEmpty();
+    list.IsEmpty(); // false
 }
 
 } // namespace examples_use_single_linked_list
